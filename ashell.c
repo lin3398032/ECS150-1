f3e917c31de9095e0d60fb3848339bc76f29bc9f
unknown <stcalica@gmail.com>
2015-04-08 13:55:24 -0700

    added ashell.c

---
+++ ashell.c
@@ -0,0 +1,110 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h> 
+#include <ctype.h>
+#include <cstring>
+
+
+#include <iostream> //for testing 
+using namespace std;
+
+
+
+void ResetCanonicalMode(int fd, struct termios *savedattributes){
+    tcsetattr(fd, TCSANOW, savedattributes);
+}
+
+void SetNonCanonicalMode(int fd, struct termios *savedattributes){
+    struct termios TermAttributes;
+    char *name;
+    
+    // Make sure stdin is a terminal. 
+    if(!isatty(fd)){
+        fprintf (stderr, "Not a terminal.\n");
+        exit(0);
+    }
+    
+    // Save the terminal attributes so we can restore them later. 
+    tcgetattr(fd, savedattributes);
+    
+    // Set the funny terminal modes. 
+    tcgetattr (fd, &TermAttributes);
+    TermAttributes.c_lflag &= ~(ICANON | ECHO); // Clear ICANON and ECHO. 
+    TermAttributes.c_cc[VMIN] = 1;
+    TermAttributes.c_cc[VTIME] = 0;
+    tcsetattr(fd, TCSAFLUSH, &TermAttributes);
+}
+
+
+int main(int argc, char *argv[]){
+    struct termios SavedTermAttributes;
+    char RXChar;
+	SetNonCanonicalMode(STDIN_FILENO, &SavedTermAttributes);
+	char buffer [250];
+	cout << "$PATH > " ; cout.flush();
+    while(1){
+	
+        read(STDIN_FILENO, &RXChar, 1);
+        if(0x04 == RXChar){ // Ctrl - D 
+            break;
+		}
+		if(0x0A == RXChar){
+			cout << "\r\n$PATH>\r"; cout.flush();
+
+			
+		}
+		//Check for up and down
+		//space
+		if(0x1B == RXChar){
+			//left bracket
+			read(STDIN_FILENO, &RXChar, 1);
+			if(0x5B == RXChar){
+				read(STDIN_FILENO, &RXChar, 1);
+				//up
+				if(0x41 == RXChar){
+					cout << "Up\t";
+					cout.flush();
+
+				}
+				//down
+				else if(0x42 == RXChar){
+					cout << "down\t";
+					cout.flush();
+
+				}
+			}
+		}
+
+		//not any of this is then a character 
+        else{
+            if(isprint(RXChar)){
+		//check for backspace
+				write(STDIN_FILENO, &RXChar, sizeof(RXChar));
+            }
+			else if(0x08 == RXChar || 0x7F == RXChar){
+				cout<< "\b \b";
+				cout.flush();
+				
+			}
+            else{
+                cout << RXChar ;
+				cout.flush();
+            }
+        }
+ 	}
+    
+    ResetCanonicalMode(STDIN_FILENO, &SavedTermAttributes);
+	cout << "\n" << endl;
+    return 0;
+}
+
+
+
+
+
+    	//read char out and also write to file? If enter is pressed look if it actually records something?
+		//http://www.csee.umbc.edu/portal/help/theory/ascii.txt
+		//http://www.lafn.org/~dave/linux/terminalIO.html
+		//http://uw714doc.sco.com/en/SDK_sysprog/TDC_Non-CanonicalModeInProc.html
+		//http://stephen-brennan.com/2015/01/16/write-a-shell-in-c/
