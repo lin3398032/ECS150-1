2f24c49f090dab9383a84468e5b3166f42cec3aa
stcalica <kycalica@gmail.com>
2015-05-14 20:13:05 -0700

    create context switch

--- P2/VirtualMachine.cpp
+++ P2/VirtualMachine.cpp
@@ -215,6 +215,7 @@ TVMStatus VMThreadCreate(TVMThreadEntry entry, void *param, TVMMemorySize memsiz
 	thread->priority = prio;
 	thread->state = VM_THREAD_STATE_DEAD;
 	thread->base = new uint8_t[thread->memsize];
+	MachineContextCreate(&(thread->context), *(thread->entry), thread->params, thread->base, thread->memsize); 		
 	all[thread->id] = thread;//added to map 	
 	cout << "memsize from app " << memsize << endl;
 	cout << "check map: " << " prio " << all[*tid]->priority << " memsize " << all[*tid]->memsize << endl;
